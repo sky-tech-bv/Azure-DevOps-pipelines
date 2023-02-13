5be0f47f99e868d16a5b27c835bbd5b2952a3532
sky-tech-bv <b.vlasenko.tech@gmail.com>
2023-02-13 18:46:20 +0200

    v18

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -6,6 +6,84 @@
 # s3: terraform-backend-state-skydev-123
 # Access key id: AKIAZKDNCVSWUON7DOHN
 
+# terraform {
+#   backend "s3" {
+#     bucket = "mybucket" # Will be overridden from build
+#     key    = "path/to/my/key" # Will be overridden from build
+#     region = "us-east-1"
+#   }
+# }
+
+# resource "aws_default_vpc" "default" {
+
+# }
+
+# data "aws_subnet_ids" "subnets" {
+#   vpc_id = aws_default_vpc.default.id
+# }
+
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster-test.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster-test.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.cluster-test.token
+#   # version                = "~> 2.10"
+# }
+
+# module "skytechbv-cluster1" {
+#   source          = "terraform-aws-modules/eks/aws"
+#   cluster_name    = "skytechbv-cluster1"
+#   cluster_version = "1.24"
+#   subnet_ids         = ["subnet-0ef0d5508555e7946", "subnet-0cb15d0898267c776"] #CHANGE
+#   # subnets = data.aws_subnet_ids.subnets.ids
+#   vpc_id          = aws_default_vpc.default.id
+
+#   #vpc_id         = "vpc-0d098073d3eb2d578"
+
+#     eks_managed_node_groups = {
+#       test_node = {
+#         min_size     = 3
+#         max_size     = 5
+#         desired_size = 3
+
+#         instance_type = "t2.micro"
+#       }
+#     }
+# }
+
+# data "aws_eks_cluster" "cluster-test" {
+#   name = module.skytechbv-cluster1.cluster_id
+# }
+
+# data "aws_eks_cluster_auth" "cluster-test" {
+#   name = module.skytechbv-cluster1.cluster_id
+# }
+
+
+# # We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
+# # ServiceAccount needs permissions to create deployments 
+# # and services in default namespace
+# resource "kubernetes_cluster_role_binding" "example" {
+#   metadata {
+#     name = "fabric8-rbac"
+#   }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "cluster-admin"
+#   }
+#   subject {
+#     kind      = "ServiceAccount"
+#     name      = "default"
+#     namespace = "default"
+#   }
+# }
+
+# # Needed to set the default region
+# provider "aws" {
+#   region  = "us-east-1"
+# }
+
+
 terraform {
   backend "s3" {
     bucket = "mybucket" # Will be overridden from build
@@ -23,45 +101,40 @@ data "aws_subnet_ids" "subnets" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster-test.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster-test.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster-test.token
-  # version                = "~> 2.10"
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  version                = "~> 2.12"
 }
 
-module "skytechbv-cluster1" {
+module "sky-cluster" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "skytechbv-cluster1"
-  cluster_version = "1.24"
-  subnet_ids         = ["subnet-0ef0d5508555e7946", "subnet-0cb15d0898267c776"] #CHANGE
+  cluster_name    = "sky-cluster"
+  cluster_version = "1.14"
+  subnets         = ["subnet-0ef0d5508555e7946", "subnet-0cb15d0898267c776"] #CHANGE
   #subnets = data.aws_subnet_ids.subnets.ids
   vpc_id          = aws_default_vpc.default.id
 
-  #vpc_id         = "vpc-0d098073d3eb2d578"
+  #vpc_id         = "vpc-1234556abcdef"
 
-    eks_managed_node_groups = {
-      test_node = {
-        min_size     = 3
-        max_size     = 5
-        desired_size = 3
-
-        instance_type = "t2.micro"
-      }
+  node_groups = [
+    {
+      instance_type = "t2.micro"
+      max_capacity  = 5
+      desired_capacity = 3
+      min_capacity  = 3
     }
+  ]
 }
 
-data "aws_eks_cluster" "cluster-test" {
-  name = module.skytechbv-cluster1.cluster_id
+data "aws_eks_cluster" "cluster" {
+  name = module.sky-cluster.cluster_id
 }
 
-data "aws_eks_cluster_auth" "cluster-test" {
-  name = module.skytechbv-cluster1.cluster_id
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.sky-cluster.cluster_id
 }
 
-
-# We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
-# ServiceAccount needs permissions to create deployments 
-# and services in default namespace
 resource "kubernetes_cluster_role_binding" "example" {
   metadata {
     name = "fabric8-rbac"
